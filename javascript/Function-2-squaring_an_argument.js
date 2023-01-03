21b3c609e1be1cf8f31919bb7ab7f980484fa1ac
fazzy12 <kaluifeanyi00@gmail.com>
2023-01-03 23:46:10 +0100

    make javascript

---
+++ javascript/Function-2-squaring_an_argument.js
@@ -0,0 +1,25 @@
+
+/*
+The code defines a function called "square" which takes a single argument "num".
+
+The function uses the "return" keyword to return the result of the expression "num * num". This expression calculates the square of the number passed as an argument to the function.
+
+The function uses the arrow function syntax, which consists of the "const" keyword, followed by the function name "square", a list of parameters inside parentheses "num", and the function body inside curly braces "{ }". The function body consists of a single line of code which calculates the square of the number and returns the result.
+
+When the function is called, it will take a single argument which will be passed to the "num" parameter. The function will then calculate the square of this number and return the result.
+*/
+
+
+//square functions
+
+const square = (num) => {
+   return num * num 	
+}
+
+
+//the function bellow is using the classic javascript function to solve the same problem
+
+function square(num){
+  return num * num
+}
+
