83ad0352362b8a69a36b95ef2a0846c731d8096b
fazzy12 <kaluifeanyi00@gmail.com>
2023-01-04 15:44:31 +0100

    Is this a triangle solved

--- javascript/Is_this_a_triangle.js
+++ javascript/Is_this_a_triangle.js
@@ -1,3 +1,10 @@
+/*
+ *INSTRUCTIONS
+ * Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+In this case, all triangles must have surface greater than 0 to be accepted).
+*/
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //Here is an implementation of a function that returns whether it is possible to build a triangle with the given side lengths:
 
 
